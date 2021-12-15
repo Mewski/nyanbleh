2fb5ba0a68b2074213650c2e55235d3c3eae517f
Anna Kitsuki <mewski813@gmail.com>
2021-12-15 15:29:11 -0600

    Added Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="navbar">
+        <a class="title">nyanbleh</a>
+        <a href="https://github.com/Mewski">
+            <nuxt-img src="/a.png"/>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+    .navbar {
+        background-color: #0E0F19;
+        height: 40px;
+        width: 100%;
+        position: absolute;
+    }
+    .title {
+        color: #ffffff;
+        font-size: 14px;
+        margin-top: 12px;
+        margin-left: 12px;
+        position: absolute;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <Navbar/>
 </template>
 
 <script>
@@ -7,3 +7,15 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+* {
+  transition: 0.15s;
+  font-family: "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+}
+body {
+  background-color: #14151F;
+  margin: 0;
+  padding: 0;
+}
+</style>
